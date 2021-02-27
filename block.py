3c6bf2b337643a445653947384870c78f3cf0756
maninserg <manin.serg@gmail.com>
2021-02-27 20:41:00 +0300

    Add save blocks with right names

--- block.py
+++ block.py
@@ -4,11 +4,15 @@ import os
 
 def write_block(name, amount, to_whom, prev_hash=""):
     blockchain_dir = os.curdir + "/blockchain/"
+    list_blocks = os.listdir(blockchain_dir)
+    list_blocks = sorted([int(x) for x in list_blocks])
+    last_block = list_blocks[-1]
+    cur_block = str(last_block + 1)
     data = {"name": name,
             "amount": amount,
             "to_whom": to_whom,
             "hash": prev_hash}
-    with open (blockchain_dir + "test", "w") as fl:
+    with open (blockchain_dir + cur_block, "w") as fl:
         json.dump(data, fl, indent=4, ensure_ascii=False)
 
 
