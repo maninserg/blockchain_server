c20f5e59764d62f7256bdbe5f3f918ada499d711
maninserg <manin.serg@gmail.com>
2021-02-27 20:19:33 +0300

    Change func for writing of blocks

--- block.py
+++ block.py
@@ -1,17 +1,19 @@
 import json
+import os
 
 
-def write_block():
-    data = {"name": "ivan",
-            "amount": 5,
-            "to_whom": "vasja",
-            "hash": "123"}
-    with open ("test", "w") as fl:
+def write_block(name, amount, to_whom, prev_hash=""):
+    blockchain_dir = os.curdir + "/blockchain/"
+    data = {"name": name,
+            "amount": amount,
+            "to_whom": to_whom,
+            "hash": prev_hash}
+    with open (blockchain_dir + "test", "w") as fl:
         json.dump(data, fl, indent=4, ensure_ascii=False)
 
 
 def main():
-    write_block()
+    write_block(name="ivan", amount=124, to_whom="serg", prev_hash="3423dfd")
 
 if __name__ == "__main__":
     main()
