b8400d88c7410fb237384eb391868e6f084bd606
maninserg <manin.serg@gmail.com>
2021-02-28 23:30:20 +0300

    Add func the getting of hash

--- block.py
+++ block.py
@@ -1,5 +1,12 @@
 import json
 import os
+import hashlib
+
+
+def get_hash(filename):
+    blockchain_dir = os.curdir + "/blockchain/"
+    fl = open(blockchain_dir + filename, "rb").read()
+    return hashlib.sha256(fl).hexdigest()
 
 
 def write_block(name, amount, to_whom, prev_hash=""):
@@ -8,16 +15,18 @@ def write_block(name, amount, to_whom, prev_hash=""):
     list_blocks = sorted([int(x) for x in list_blocks])
     last_block = list_blocks[-1]
     cur_block = str(last_block + 1)
+    prev_hash = get_hash(str(last_block))
     data = {"name": name,
             "amount": amount,
             "to_whom": to_whom,
             "hash": prev_hash}
-    with open (blockchain_dir + cur_block, "w") as fl:
+    with open(blockchain_dir + cur_block, "w") as fl:
         json.dump(data, fl, indent=4, ensure_ascii=False)
 
 
 def main():
-    write_block(name="ivan", amount=124, to_whom="serg", prev_hash="3423dfd")
+    write_block(name="vladimir", amount=210, to_whom="kukuz")
+
 
 if __name__ == "__main__":
     main()
